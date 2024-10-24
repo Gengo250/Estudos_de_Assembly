00f3971b5d40b100a1f1e3b5fa58281818a5524c
_@_Gengo_@_ <miguelgengo28@gmail.com>
2024-10-24 19:41:59 -0300

    testes atualizados

--- Treino_de_Assembly/Testes_Assembly/TESTE_4.asm
+++ Treino_de_Assembly/Testes_Assembly/TESTE_4.asm
@@ -1,15 +1,33 @@
 TITLE ASSEMBLY-PROVA
 .MODEL SMALL 
 .DATA
-    MSG DB 10,13,"PROVA 1 OSC. $"
+        MSG1 db 'ax eh maior', 13,10,'$'
+        MSG2 db 'bx eh maior', 13,10, '$'
+
 .CODE
-MAIN PROC 
-    MOV AX, @DATA 
-    MOV DS, AX
-    MOV AH, 09
-    MOV DX, OFFSET MSG 
-    INT 21H
-    MOV AH, 4ch
-    INT 21H
+MAIN PROC
+        mov ax, @DATA
+        mov ds, ax
+        mov ax, 7FFFH
+        mov bx, 8000H
+        mov cx, ax
+        cmp ax, bx
+        jg maior
+        lea dx, MSG2
+        mov ah, 09h
+        int 21h
+        mov cx,bx
+        jmp exit
+
+        maior:
+                lea dx, MSG1
+                mov ah, 09h
+                int 21h 
+        exit:
+                mov ah, 4Ch
+                int 21h        
+
+       
+    
 MAIN ENDP
 END MAIN 
