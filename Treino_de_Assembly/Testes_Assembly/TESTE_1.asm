996644e33c831d64aa86dd071e1621520b4da6fc
_@_Gengo_@_ <miguelgengo28@gmail.com>
2024-10-27 11:51:04 -0300

    tabela ascii e atualizações

--- Treino_de_Assembly/Testes_Assembly/TESTE_1.asm
+++ Treino_de_Assembly/Testes_Assembly/TESTE_1.asm
@@ -2,24 +2,44 @@ TITLE mensagem
 .MODEL SMALL 
 .STACK 100H
 .DATA
-    MSG1 DB "ROLA. $"
-    MSG2 DB 10,13,"PICA. $"
+    MSG1 DB 10,13,"Digite o primeiro numero: $"
+    MSG2 DB 10,13,"Digite o segungo numero:  $"
+    MSG3 DB 10,13,"Resultado: $"
 
 .CODE
 MAIN PROC
-         MOV AX,@DATA
-         MOV DS,AX
+  mov ax, @DATA
+  mov ds, ax 
 
-         MOV AH,9
-         LEA DX,MSG1
-         INT 21h
+  mov ah, 9
+  lea dx, MSG1
+  int 21h 
 
-         MOV AH,9
-         LEA DX,MSG2
-         INT 21h
+  mov ah, 1
+  int 21h 
 
-         MOV AH,4Ch
-         INT 21h
+  mov bl, AL
+
+  mov ah, 9
+  lea dx, MSG2 
+  int 21h 
+
+  mov ah, 1 
+  int 21h 
+
+  add bl, AL
+  sub bl, '0'
+
+  mov ah, 9
+  lea dx, MSG3
+  int 21h 
+
+  mov dl, bl 
+  mov ah, 2 
+  int 21h 
+
+  mov ah, 4Ch
+  int 21h 
 
 MAIN ENDP
 END MAIN
--- Treino_de_Assembly/Testes_Assembly/TESTE_2.asm
+++ Treino_de_Assembly/Testes_Assembly/TESTE_2.asm
@@ -1,13 +1,37 @@
 TITLE TESTE
 .MODEL SMALL
+.DATA 
+ MSG1 DB 10,13,"DIGITE UMA LETRA MAIUSCULA: $"
+ MSG2 DB 10,13,"SUA LETRA minuscula CORRESPONDENTE: $"
 .CODE
 MAIN PROC
- mov dl, '?'
- mov ah, 2
- int 21h
- 
- mov ah, 4ch
- int 21h
+    mov ax, @DATA ;acessa o segmento .DATA
+    mov ds, ax 
+
+    mov ah, 9
+    lea dx, MSG1 ;exibe a mensagem 1
+    int 21h
+
+    mov ah, 1 ;usuario digita o caracter e salva em AL
+    int 21h
+
+    mov bl, AL ;move o conteúdo para bl
+
+    add bl, 32 ;adiciona 32 em bl para transformar a letra maiuscula para minuscula 
+
+    mov ah, 9
+    lea dx, MSG2 ;exibe a mensagem 2
+    int 21h 
+
+    mov ah, 2 
+    mov dl, bl ;exibe o resultado esperado
+    int 21h
+
+
+
+    mov ah, 4Ch ;encerra o programa
+    int 21h 
+
 MAIN ENDP
     END MAIN 
 
